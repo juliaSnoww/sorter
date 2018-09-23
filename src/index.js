ffd3413d0d0e91d5feb4b0d578da74e445b512d7
Julia Pritychenko <juliasnoww@mail.ru>
2018-09-23 15:49:28 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,30 +1,38 @@
 class Sorter {
   constructor() {
-    // your implementation
+    this.box=[];
   }
 
   add(element) {
-    // your implementation
+      this.box.push(element);
   }
 
   at(index) {
-    // your implementation
+      return this.box[index]
   }
 
   get length() {
-    // your implementation
+      return this.box.length;
   }
 
   toArray() {
-    // your implementation
+    return this.box;
   }
 
   sort(indices) {
-    // your implementation
+    var andrey = [];
+    indices.sort(function(left, right) {return left - right});
+    for (let i of indices) {
+      andrey.push(this.box[i]);
+    }
+    andrey.sort(function(left, right) {return left - right});
+    for (let i of indices) {
+      this.box[i] = andrey.shift();
+    }
   }
 
-  setComparator(compareFunction) {
-    // your implementation
+  setComparator(compareFunction){
+      this.comparator = compareFunction;
   }
 }
 
